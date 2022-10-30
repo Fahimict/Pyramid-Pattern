86c17b67867dadfc6b40385b6217f50b9eee3d70
Fahimict <fa9010583@gmail.com>
2022-10-30 22:33:00 +0600

    Add files via upload

---
+++ HalfPyramid.c
@@ -0,0 +1,15 @@
+///Half Pyramid of *
+#include<stdio.h>
+int main()
+{
+    int row,col;
+    for(row=1;row<=5;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
